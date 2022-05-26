e94a15443a2d1618ff77218ffdb984448370c0da
shinichihatano <monntatarou2008@gmail.com>
2022-05-26 21:40:39 +0900

    TimePickerDialog実装 時刻により挨拶を変化させる　実装

--- app/src/main/java/com/example/aisatsuapp/MainActivity.kt
+++ app/src/main/java/com/example/aisatsuapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.aisatsuapp
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        showTimePickerDialog()
+
+    }
+
+    private fun showTimePickerDialog(){
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener{view, hour ,minute ->
+                if( hour >= 18 && minute >= 0) {
+                    //textview1.text = "$hour:$minute"
+                    textview1.text = "こんばんは"
+                }else if(hour >= 10 && minute >= 0){
+                    textview1.text = "こんにちは"
+                }else if(hour >= 2 && minute >= 0){
+                    textview1.text = "おはよう"
+                }
+            },
+            13, 0, true)
+        timePickerDialog.show()
     }
 }
